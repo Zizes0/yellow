6c13368df154bfb8a303e597107de52d5d7ad566
Zizes01 <92364691+Zizes0@users.noreply.github.com>
2024-02-08 16:26:31 +0100

    Add files via upload

--- job/js/script.js
+++ job/js/script.js
@@ -1,21 +1,50 @@
 $('.slider').bxSlider({
-  pager: false,
+  pagerCustom: '.custom-controls',
   minSlides: 2,
   maxSlides: 5,
   slideWidth: 208,
   slideMargin: 18,
   moveSlides: 1,
-  prevText: '<span class="custom-prev">Previous</span>',
-  nextText: '<span class="custom-next">Next</span>',
+  prevText: '<img src="../images/previous.png" alt="Previous" class="custom-prev">',
+  nextText: '<img src="../images/next.png" alt="Next" class="custom-next">',
   onSliderLoad: function (currentIndex) {
-    $('.slider-box:visible').not('.bx-clone').eq(2).addClass('slider-center');
+    var $thirdBox = $('.slider-box:visible').not('.bx-clone').eq(2);
+    $thirdBox.css({background: 'white',
+    border: '2px solid yellow'});
+    $thirdBox.addClass('slider-center');
   },
   onSlideNext: function ($slideElement, oldIndex, newIndex) {
+    $('.slider-box').css({
+      background: 'transparent'
+    });
+    $('.slider-box').css({
+      border: '2px solid white'
+    });
+  
+    var $centeredBox = $slideElement.next().next();
+    $centeredBox.css({
+      background: 'white',
+      border: '2px solid yellow'
+      
+    });
+  
     $('.slider-box').removeClass('slider-center');
-    $slideElement.next().next().addClass('slider-center');
+    $centeredBox.addClass('slider-center');
   },
   onSlidePrev: function ($slideElement, oldIndex, newIndex) {
+    $('.slider-box').css({
+      background: 'transparent'
+    });
+    $('.slider-box').css({
+      border: '2px solid white'
+    });
+    var $centeredBox = $slideElement.next().next();
+    $centeredBox.css({
+      background: 'white',
+      border: '2px solid yellow'
+    });
     $('.slider-box').removeClass('slider-center');
-    $slideElement.next().next().addClass('slider-center');
-  }
+    $centeredBox.addClass('slider-center');
+  },
 });
+
